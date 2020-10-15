51f44ce8e15a867edbc71cdc6afc2f577e4044b5
Tim Brouwer <timbrouwer@protonmail.com>
2020-10-15 07:30:24 +0200

    add content for categories

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -14,19 +14,23 @@
 <script lang="ts">
 import CategoryCard from './categories/CategoryCard.vue'
 
+// This is a stub, should load from server
+import cultuur from '@/content/cultuur.json'
+import diy from '@/content/diy.json'
+import ontspanning from '@/content/ontspanning.json'
+import tech from '@/content/tech.json'
+import template from '@/content/template.json'
+
 
 export default {
   name: 'Categories',
   components: {
     CategoryCard
   },
-  data() {
-    return {
-      categories: [ 
-          { name: "Tech" },
-          { name: "Cultuur" },
-          { name: "Doe het zelf" },
-          { name: "Ontspanning" }
+  computed: {
+    categories() {
+      return [
+        cultuur, diy, ontspanning, tech, template
       ]
     }
   }
