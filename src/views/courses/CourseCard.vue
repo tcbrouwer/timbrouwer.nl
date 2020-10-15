39fd8d02491baca22b87cd35684c64eaccddf27d
Tim Brouwer <timbrouwer@protonmail.com>
2020-10-15 22:25:22 +0200

    computed data

--- src/views/courses/CourseCard.vue
+++ src/views/courses/CourseCard.vue
@@ -1,24 +1,24 @@
 <template>
   <b-card
-    :img-src="trainer().avatar"
+    :img-src="trainer.avatar"
     img-alt="vue"
     img-top
     style="min-width: 15rem; max-width: 25rem"
     class="mb-5"
   >
-    <b-card-title>{{ course.name }}</b-card-title>
-    <b-card-sub-title class="mb-2">Trainer: {{ trainer().name }}</b-card-sub-title>
+    <b-card-title>{{ name }}</b-card-title>
+    <b-card-sub-title class="mb-2">Trainer: {{ trainer.name }}</b-card-sub-title>
     <b-card-text>
-      {{ description() }}
+      {{ description }}
     </b-card-text>
     <b-list-group flush>
-      <b-list-group-item>Doelgroep: {{ audience() }}</b-list-group-item>
-      <b-list-group-item>Duur: {{ duration() }}</b-list-group-item>
-      <b-list-group-item>Moment: {{ start() }}</b-list-group-item>
-      <b-list-group-item>Kosten: {{ cost() }}</b-list-group-item>
+      <b-list-group-item>Doelgroep: {{ audience }}</b-list-group-item>
+      <b-list-group-item>Duur: {{ duration }}</b-list-group-item>
+      <b-list-group-item>Moment: {{ start }}</b-list-group-item>
+      <b-list-group-item>Kosten: {{ cost }}</b-list-group-item>
     </b-list-group>
     <template v-slot:footer>
-      <a :href="link().url">{{ link().text }}</a>
+      <a :href="link.url">{{ link.text }}</a>
     </template>
   </b-card>
 </template>
@@ -27,44 +27,45 @@
 export default {
   name: "CourseCard",
   props: ["course"],
-  data() {
-    return {
-      audience: () => {
-        return this.course.audience ? this.course.audience : "Iedereen"
-      },
-      cost: () => {
-        return this.course.cost ? this.course.cost: "Onbekend"
-      },
-      description: () => {
-        return this.course.description ? this.course.description : "Geen beschrijving beschikbaar"
-      },
-      duration: () => {
-        return this.course.duration ? this.course.duration : "Onbekend"
-      },
-      link: () => {
-        if( this.course.link === undefined ) {
-          return { url: "", text: "Nog geen link" }
-        } else {
-          return {
-            url: this.course.link.url ? this.course.link.url : "",
-            text: this.course.link.text ? this.course.link.text : "Meer informatie"
-          }
+  computed: {
+    audience() {
+      return this.course.audience ? this.course.audience : "Iedereen"
+    },
+    cost() {
+      return this.course.cost ? this.course.cost: "Onbekend"
+    },
+    description() {
+      return this.course.description ? this.course.description : "Geen beschrijving beschikbaar"
+    },
+    duration() {
+      return this.course.duration ? this.course.duration : "Onbekend"
+    },
+    link() {
+      if( this.course.link === undefined ) {
+        return { url: "", text: "Nog geen link" }
+      } else {
+        return {
+          url: this.course.link.url ? this.course.link.url : "",
+          text: this.course.link.text ? this.course.link.text : "Meer informatie"
         }
-      },
-      trainer: () => {
-        if( this.course.trainer === undefined ) {
-          return { name: "Onbekend", avatar: this.randomPic() }
-        } else {
-          return {
-            name: this.course.trainer.name ? this.course.trainer.name : "Onbekend",
-            avatar: this.course.trainer.avatar ? this.course.trainer.avatar : this.randomPic()
-          }
+      }
+    },
+    name() {
+      return this.course.name ? this.course.name : "" // name is used as key, so it should not be missing
+    },
+    start() {
+      return this.course.start ? this.course.start : "Onbekend"
+    },
+    trainer() {
+      if( this.course.trainer === undefined ) {
+        return { name: "Onbekend", avatar: this.randomPic() }
+      } else {
+        return {
+          name: this.course.trainer.name ? this.course.trainer.name : "Onbekend",
+          avatar: this.course.trainer.avatar ? this.course.trainer.avatar : this.randomPic()
         }
-      },
-      start: () => {
-        return this.course.start ? this.course.start : "Onbekend"
-      },      
-    };
+      }
+    },      
   },
   methods: {
     randomPic() {
