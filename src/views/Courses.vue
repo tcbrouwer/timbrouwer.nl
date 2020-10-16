dd693abab8eda00e06be862f04ca91c18adecd0a
Tim Brouwer <timbrouwer@protonmail.com>
2020-10-16 17:44:05 +0200

    better course keys

--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -1,45 +1,50 @@
 <template>
   <div>
     <h1>Cursus aanbod (Concept)</h1>
-    
+
     <div>
       <b-card-group class="m-3" deck>
-        <CourseCard v-for="course in courses" :key=course.name
-          :course = course
+        <CourseCard
+          v-for="course in courses"
+          :key="courseKey( course )"
+          :course="course"
         />
       </b-card-group>
-    </div>  
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import CourseCard from '@/components/CourseCard.vue'
+import CourseCard from "@/components/CourseCard.vue";
 
 // This is a stub, should load from server
-import code4nl from '@/content/code4nl.json'
-import courses from '@/content/courses.json'
-import externals from '@/content/externals.json'
+import code4nl from "@/content/code4nl.json";
+import courses from "@/content/courses.json";
+import externals from "@/content/externals.json";
 
 export default {
-  name: 'Courses',
+  name: "Courses",
   components: {
     CourseCard,
   },
   data() {
     return {
-      courses: []
-    }
+      courses: [],
+    };
   },
   mounted() {
-    this.getCourses()
+    this.getCourses();
   },
   methods: {
+    courseKey( course ) {
+       return course.name.short ? course.name.short : course.name 
+    },
     getCourses() {
       // This should become an API call to server
-      this.courses = courses.concat( externals ).concat( code4nl )
-    }
-  }
-}
+      this.courses = courses.concat(externals).concat(code4nl);
+    },
+  },
+};
 </script>
 
 <style scoped>
